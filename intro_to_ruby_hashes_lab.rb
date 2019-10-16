ae5607ad3e218fa7c10a85b84d5457fb674d1911
Ali Saidik <saida130.210@gmail.com>
2019-10-16 20:42:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ end
 def monopoly_with_second_tier
   { 
     premier_league: {},
-    la_liga; "barcelona"
+    la_liga: "barcelona"
   }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
